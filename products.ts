f56c06c2063b04c78da921b98562b83c5f78fb8a
adrian <adrianribeiro837@gmail.com>
2026-04-28 22:03:34 -0300

    Adicionando novas imagens de produtos e atualizando componentes

--- products.ts
+++ products.ts
@@ -13,6 +13,31 @@ import product12 from "@/assets/product-12.jpg";
 import product13 from "@/assets/product-13.jpg";
 import product14 from "@/assets/product-14.jpg";
 import product15 from "@/assets/product-15.jpg";
+import product16 from "@/assets/product-16;jpg";
+import product17 from "@/assets/product-17.jpg";
+import product18 from "@/assets/product-18.jpg";
+import product19 from "@/assets/product-19.jpg";
+import product20 from "@/assets/product-20.jpg";
+import product21 from "@/assets/product-21.jpg";
+import product22 from "@/assets/product-22.jpg";
+import product23 from "@/assets/product-23.jpg";
+import product24 from "@/assets/product-24.jpg";
+import product25 from "@/assets/product-25.jpg";
+import product26 from "@/assets/product-26.jpg";
+import product27 from "@/assets/product-27.jpg";
+import product28 from "@/assets/product-28.jpg";
+import product29 from "@/assets/product-29.jpg";
+import product30 from "@/assets/product-30.jpg";
+import product31 from "@/assets/product-31.jpg";
+import product32 from "@/assets/product-32.jpg";
+import product33 from "@/assets/product-33.jpg";
+import product34 from "@/assets/product-34.jpg";
+import product35 from "@/assets/product-35.jpg";
+import product36 from "@/assets/product-36.jpg";
+import product37 from "@/assets/product-37.jpg";
+
+
+
 
 
 
@@ -168,7 +193,7 @@ export const products: Product[] = [
     description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
     images: [product14],
     category: "Sutiãs",
-    sizes: ["P", "M", "G"],
+    sizes: ["G"],
     details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
   },
   {
@@ -180,7 +205,230 @@ export const products: Product[] = [
     category: "Corset",
     sizes: ["P", "M", "G"],
     details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "16",
+    name: "Conjunto obsessão premium preto ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product16],
+    category: "Conjuntos",
+    sizes: ["M"],
+    details: ["Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "17",
+    name: "Conjunto de tule bordado Isis ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product17],
+    category: "Conjuntos",
+    sizes: ["M", "G"],
+    details: [ "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "18",
+    name: "Conjunto angel vermelho disponível ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product18],
+    category: "Conjuntos",
+    sizes: ["M"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "19",
+    name: "Sutiã reforçado vermelho disponível",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product19],
+    category: "Sutiãs",
+    sizes: ["GG"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "20",
+    name: "Sutiã reforçado renda vinho ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product20],
+    category: "Sutiãs",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "21",
+    name: "Camisola de renda heloá ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product21],
+    category: "Camisolas",
+    sizes: ["M"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "22",
+    name: "Conjunto Laila vermelho",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product22],
+    category: "Conjuntos",
+    sizes: ["G", "GG"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "23",
+    name: "Conjunto elegance P&B ",
+    price: 119.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product23],
+    category: "Conjuntos",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "24",
+    name: "Conjunto ravena de renda pink ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product24],
+    category: "Conjuntos",
+    sizes: ["P"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "25",
+    name: "Conjunto ravena de renda vermelho",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product25],
+    category: "Conjuntos",
+    sizes: ["M"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "26",
+    name: "Camisola sensualité + calcinha",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product26],
+    category: "Camisolas",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "27",
+    name: "Conjunto rosa romance",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product27],
+    category: "Conjuntos",
+    sizes: ["P" ],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "28",
+    name: "Conjunto elegance pink",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product28],
+    category: "Conjuntos",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "29",
+    name: "Sutiã reforçado azul marinho",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product29],
+    category: "Sutiãs",
+    sizes: ["GG"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "30",
+    name: "Conjunto preto floral",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product30],
+    category: "Conjuntos",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "31",
+    name: "Conjunto Lara ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product31],
+    category: "Conjuntos",
+    sizes: ["M"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "32",
+    name: "Conjunto seduza-me",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product32],
+    category: "Conjuntos",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "33",
+    name: "Conjunto Laila branco ",
+    price: 99.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product33],
+    category: "Conjuntos",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "34",
+    name: "Sutiã reforçado azul frozen ",
+    price: 59.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product34],
+    category: "Sutiãs",
+    sizes: ["G", "M"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "35",
+    name: "Baby doll cherry ",
+    price: 59.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product35],
+    category: "Baby Doll" ,
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "36",
+    name: "Conjunto sedução ",
+    price: 59.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product36],
+    category: "Conjuntos",
+    sizes: ["G"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
+  },
+  {
+  id: "37",
+    name: "Conjunto sedução",
+    price: 59.90,
+    description: "Variação do nosso best-seller com detalhes exclusivos em renda e acabamento dourado.",
+    images: [product37],
+    category: "Conjuntos",
+    sizes: ["M"],
+    details: ["Renda francesa", "Detalhes dourados", "Bojo estruturado", "Conforto o dia todo"],
   }
+  
+
+
 ];
 
 export const categories = ["Todos", "Sutiãs", "Conjuntos", "Corset", "Baby Doll" ,"Pijamas" , "Linha Noite" , "Pantufas"];
